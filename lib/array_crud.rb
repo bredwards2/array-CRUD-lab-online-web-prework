7beb6d995e8c8e566313305ad584bc734bd5c523
Britny Edwards <bredwards2@gmail.com>
2018-11-12 01:57:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
- create_an_empty_array = []
+ array = []
 end
 
 def create_an_array
-  create_an_array = array
+  array = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
